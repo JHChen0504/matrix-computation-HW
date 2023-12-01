d56c7bc659821848f6c354175d8e272a20a777d7
JHChen0504 <119959373+JHChen0504@users.noreply.github.com>
2023-12-02 03:27:07 +0800

    Update HW.m

--- HW.m
+++ HW.m
@@ -4,16 +4,16 @@ filename = 'out.ego-facebook';
 main_HW1_1;
 main_HW_1;
 main_HW_2;
-save mtx_10k cputime lambda ev residual -mat
+save mtx_10k.mat cputime lambda ev residual -mat
 
 %% filename = matrix with dimension 100k
 filename = 'out.ego-twitter';
 main_HW1_1;
 % main_HW_1; % Takes HUGE amount of time, use if needed.
 main_HW_2;
-save mtx_100k cputime lambda ev residual -mat
+save mtx_100k.mat cputime lambda ev residual -mat
 
 %% filename = matrix with dimension 1000k
 filename = 'out.roadNet-PA';
 main_HW_2;
-save mtx_1000k cputime lambda ev residual -mat
+save mtx_1000k.mat cputime lambda ev residual -mat
