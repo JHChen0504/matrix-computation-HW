e6c0c51cd0a39c50f58524ee9c962739e533f2e7
JHChen0504 <119959373+JHChen0504@users.noreply.github.com>
2023-12-12 11:35:37 +0800

    Update HW.m

--- HW.m
+++ HW.m
@@ -15,5 +15,6 @@ save mtx_100k.mat cputime lambda ev residual -mat
 
 %% filename = matrix with dimension 1000k
 filename = 'out.roadNet-PA';
+main_HW1_1;
 main_HW_2;
 save mtx_1000k.mat cputime lambda ev residual -mat
