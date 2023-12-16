9f833450d9c79d3f96e9250afbcf736b58d21acb
JHChen0504 <119959373+JHChen0504@users.noreply.github.com>
2023-12-16 16:57:14 +0800

    Update main_HW3_1c.m

--- code/main_HW3_1c.m
+++ code/main_HW3_1c.m
@@ -5,4 +5,4 @@ tic;
 [sol,flag,RelRes,Iter.HW3_1c] = pcg(shift_L, b, tol, maxit, prec_M1, prec_M2);
 cputime.PCG2 = toc;
 fprintf('flag = %1.0f, RelRes = %11.4e, ITER = %6.0f, CPU_time = %11.4e \n', ...
-flag, RelRes, ITER, cputime.PCG2);
+flag, RelRes, Iter.HW3_1c, cputime.PCG2);
